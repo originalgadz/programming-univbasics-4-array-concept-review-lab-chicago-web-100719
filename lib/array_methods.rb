854a0e061662d21f1cdabcf31dd06d5584549677
Glenn A Doherty <gadz@earthlink.net>
2019-08-18 15:16:46 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
-  array.length.times do |index|
+  array.length.times { |index|
     if array[index] === value_to_find
       return index
     end 
-  end
+  }
   return nil
 end
 
